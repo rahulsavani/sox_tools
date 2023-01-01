97f502fb5fbe4a2a2806949f974d9861a9ef5e37
Rahul Savani <rahul.savani@gmail.com>
2023-01-01 11:10:28 +0000

    split_stereo.sh

---
+++ split_stereo.sh
@@ -0,0 +1,7 @@
+# loop through wav files in TMP
+# return mono file, normalised to -0.1 db
+for i in `ls TMP/*.wav`; do
+    # use left channel (remix 1 as opposed to remix 2) 
+    # since that will work even if the wav is already mono
+    sox $i -b 16 `basename $i` remix 1 norm -0.1
+done
