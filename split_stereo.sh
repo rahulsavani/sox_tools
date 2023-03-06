ae17ff68df01edbfd676c0b21bedcc7ee8e3774b
Rahul Savani <rahul.savani@gmail.com>
2023-03-06 08:20:49 +0000

    bitdepth in split_stereo.sh

--- split_stereo.sh
+++ split_stereo.sh
@@ -1,11 +1,13 @@
+#bitdepth=16
+bitdepth=24
 # loop through wav files in TMP
 # return mono file, normalised to -0.1 db
 for i in `ls TMP/*.wav`; do
     # use left channel (remix 1 as opposed to remix 2) 
     # since that will work even if the wav is already mono
-    sox $i -r 44100 -b 16 `basename $i` remix 1 norm -0.1
+    sox $i -r 44100 -b $bitdepth `basename $i` remix 1 norm -0.1
 done
 
 for i in `ls TMP2/*.aif`; do
-    sox $i -r 44100 -b 16 `basename $i`.wav remix 1 norm -0.1
+    sox $i -r 44100 -b $bitdepth `basename $i`.wav remix 1 norm -0.1
 done
